87aeb9a68acaf56cc3b6edfa4a2f45c97c004486
Mathieu HAYS <mathieu@mathieuhays.co.uk>
2017-03-19 13:08:35 +0000

    Tests - add Documentation Item, Types and Type

--- tests/test-documentation-item.php
+++ tests/test-documentation-item.php
@@ -5,3 +5,50 @@
  * Date: 18/03/2017
  * Time: 10:46
  */
+
+use SimpleDocumentation\DocumentationItems\DocumentationItem;
+use SimpleDocumentation\DocumentationItems\DocumentationItems;
+
+class DocumentationItemTests extends WP_UnitTestCase {
+	/**
+	 * Create DocumentationItem \WP_Post
+	 *
+	 * @param array $args
+	 *
+	 * @return \WP_Post
+	 */
+	public function util_create_and_get( $args = [] ) {
+		$args['post_type'] = DocumentationItems::POST_TYPE;
+		return $this->factory->post->create_and_get( $args );
+	}
+
+	public function test_get_view_link() {
+		$post = $this->util_create_and_get();
+
+		$item = new DocumentationItem( $post );
+
+		// There might be better way to test this later on...
+		$this->assertTrue( filter_var( $item->get_view_link(), FILTER_VALIDATE_URL ) );
+	}
+
+	public function test_get_edit_link() {
+		$post = $this->util_create_and_get();
+
+		$item = new DocumentationItem( $post );
+
+		// should use wordpress admin
+		$this->assertNotFalse( strpos( $item->get_edit_link(), 'post.php' ) );
+	}
+
+	public function test_get_type() {
+		// @TODO implement get_type() detect that DocumentationTypes()->get() is called
+	}
+
+	public function test_has_type() {
+		// @TODO implement has_type() test
+	}
+
+	public function test_is_highlighted() {
+		// @TODO implement is_highlighted() test
+	}
+}
