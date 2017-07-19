d0fc73f38fc860bce05018c725fa626e5d4e1c3e
Mathieu <mathieu@mathieuhays.com>
2017-07-19 09:15:55 +0200

    PostTypeColumnHelper - add size sanitiser, tests & comments

--- includes/utilities/class-post-type-column-helper.php
+++ includes/utilities/class-post-type-column-helper.php
@@ -8,19 +8,42 @@ namespace SimpleDocumentation\Utilities;
 
 class PostTypeColumnHelper {
 	/**
-	 *  @var string Post Type
+	 * @var string Post Type
 	 */
 	private $post_type;
-	private $columns = [];
-	private $before = [];
-	private $after = [];
-	private $remove = [];
-	private $done = [];
-	private $sizes = [];
-
 
 	/**
-	 *  @param  string  $post_type
+	 * @var array - List of custom columns
+	 */
+	private $columns = [];
+
+	/**
+	 * @var array[] - List of default columns having custom ones to be displayed before them.
+	 */
+	private $before = [];
+
+	/**
+	 * @var array[] - List of default columns having custom ones to be displayed after them.
+	 */
+	private $after = [];
+
+	/**
+	 * @var string[] - List of columns meant to be removed
+	 */
+	private $remove = [];
+
+	/**
+	 * @var string[] - List of columns ids that have already been processed
+	 */
+	private $done = [];
+
+	/**
+	 * @var string[] - List of custom column size (associated with their column slug)
+	 */
+	private $sizes = [];
+
+	/**
+	 * @param  string  $post_type
 	 */
 	public function __construct( $post_type = null ) {
 		$this->post_type = empty( $post_type ) ? 'post' : $post_type;
@@ -28,10 +51,9 @@ class PostTypeColumnHelper {
 		$this->bootstrap();
 	}
 
-
 	public function bootstrap() {
 		/**
-		 *  Handles which content is displayed where
+		 * Handles which content is displayed where
 		 */
 		add_action(
 			sprintf( 'manage_%s_posts_custom_columns', $this->post_type ),
@@ -41,7 +63,7 @@ class PostTypeColumnHelper {
 		);
 
 		/**
-		 *  Add/Remove Columns
+		 * Add/Remove Columns
 		 */
 		add_filter(
 			sprintf( 'manage_%s_posts_columns', $this->post_type ),
@@ -50,22 +72,20 @@ class PostTypeColumnHelper {
 		);
 
 		/**
-		 *  Add Custom CSS in case we use a custom size for the column
+		 * Add Custom CSS in case we use a custom size for the column
 		 */
 		add_action( 'admin_head', [ $this, 'handle_column_size' ] );
 	}
 
-
 	/**
-	 *  Trigger Content Callback if it has been registered with this class.
-	 *  The callback is meant to echo content.
+	 * Trigger Content Callback if it has been registered with this class.
+	 * The callback is meant to echo content.
 	 *
-	 *  @param  string  $column_slug
-	 *  @param  int     $post_id
+	 * @param  string  $column_slug
+	 * @param  int     $post_id
 	 */
 	public function handle_column_content( $column_slug, $post_id ) {
-		// Skip if haven't registered custom columns or this column is one hasn't
-		// been registered as part of this class.
+		// Skip if we haven't registered custom columns
 		if ( empty( $this->columns ) || ! isset( $this->columns[ $column_slug ] ) ) {
 			return;
 		}
@@ -73,12 +93,11 @@ class PostTypeColumnHelper {
 		call_user_func( $this->columns[ $column_slug ]['callback'], $post_id );
 	}
 
-
 	/**
-	 *  Handle Columns
+	 * Handle Columns (re-order, add, remove, etc..)
 	 *
-	 *  @param  array   $default_columns
-	 *  @return array
+	 * @param  array   $default_columns
+	 * @return array
 	 */
 	public function handle_columns( $default_columns ) {
 		// Nothing to do here so we return early
@@ -120,8 +139,7 @@ class PostTypeColumnHelper {
 		}
 
 		// Parse remaining columns
-		// use-case: Columns registered without requesting to be 'before' or 'after'
-		// an existing column
+		// use-case: Columns registered without requesting to be 'before' or 'after' an existing column
 		foreach ( $this->columns as $column_id => $column_data ) {
 			// Skip if we already handled this custom column
 			if ( in_array( $column_id, $this->done ) ) {
@@ -134,9 +152,8 @@ class PostTypeColumnHelper {
 		return $final;
 	}
 
-
 	/**
-	 *  Add CSS to admin_head to handle columns custom size
+	 * Add CSS to admin_head to handle columns custom size
 	 */
 	public function handle_column_size() {
 		if ( empty( $this->sizes ) ) {
@@ -156,17 +173,21 @@ class PostTypeColumnHelper {
 		printf( '<style>%s</style>', join( '', $output ) );
 	}
 
-
 	/**
-	 *  Register Column to be added later
+	 * Register Column to be added later
 	 *
-	 *  @param
+	 * @param string $slug_raw
+	 * @param array $options
+	 * @param callable $callback
 	 */
 	public function add( $slug_raw, $options, $callback ) {
 		$slug = sanitize_key( $slug_raw );
 
 		$option_defaults = [
 			'label' => $slug_raw,
+			'before' => null, // string - column slug
+			'after' => null, // string - column slug
+			'size' => null, // string - css value for width ex: '10px' or '10%'
 		];
 
 		$args = wp_parse_args( $options, $option_defaults );
@@ -190,11 +211,10 @@ class PostTypeColumnHelper {
 		}
 	}
 
-
 	/**
-	 *  Register Column to be removed later
+	 * Register Column to be removed later
 	 *
-	 *  @param  string  $slug
+	 * @param  string  $slug
 	 */
 	public function remove( $slug ) {
 		if ( ! in_array( $slug, $this->remove ) ) {
@@ -202,9 +222,8 @@ class PostTypeColumnHelper {
 		}
 	}
 
-
 	/**
-	 *  Append data for key in array. Creates array if not defined already
+	 * Append data for key in array. Creates array if not defined already
 	 *
 	 * @param  array   $array
 	 * @param  string  $key
@@ -219,5 +238,27 @@ class PostTypeColumnHelper {
 		$array[ $key ][] = $data;
 
 		return $array;
+	}
+
+	/**
+	 * Sanitize the size string to make sure it's a valid css value (including the unit)
+	 *
+	 * @param string $size
+	 * @return string
+	 */
+	public static function sanitize_size( $size ) {
+		if ( ! is_string( $size ) && ! is_numeric( $size ) ) {
+			return $size;
+		}
+
+		$pattern_count = preg_match( '/(p[cx]|r?em|%)$/', $size );
+
+		// return early if an error occurred w/ the regex or if we did find what we were looking for.
+		if ( $pattern_count === false ||
+		     $pattern_count > 0 ) {
+			return $size;
+		}
+
+		return $size . 'px'; // default to pixel
 	}
 }
--- tests/test-post-type-column-helper-tests.php
+++ tests/test-post-type-column-helper-tests.php
@@ -1,4 +1,5 @@
 <?php
+use SimpleDocumentation\Utilities\PostTypeColumnHelper;
 
 /**
  * Client Documentation - test-post-type-column-helper-tests.php
@@ -31,7 +32,48 @@ class PostTypeColumnHelperTests extends WP_UnitTestCase {
 		// @TODO implement test_remove
 	}
 
-	public function test_append() {
-		// @TODO implement test_append
+	public function test_append_empty_array() {
+		$array = PostTypeColumnHelper::append( [], 'test', 'data' );
+
+		$this->assertTrue( is_array( $array ) );
+		$this->assertSame( 1, count( $array ) );
+		$this->assertTrue( isset( $array['test'] ) );
+		$this->assertSame( 1, count( $array['test'] ) );
+	}
+
+	public function test_append_existing_array() {
+		$array = PostTypeColumnHelper::append( [
+			'existing' => [ 'data' ],
+		], 'test', 'data' );
+
+		$this->assertTrue( is_array( $array ) );
+		$this->assertSame( 2, count( $array ) );
+		$this->assertTrue( isset( $array['test'] ) );
+		$this->assertTrue( isset( $array['existing'] ) );
+		$this->assertSame( 1, count( $array['test'] ) );
+		$this->assertSame( 1, count( $array['existing'] ) );
+	}
+
+	public function test_append_existing_array_existing_key() {
+		$array = PostTypeColumnHelper::append( [
+			'existing' => [ 'data' ],
+		], 'existing', 'data' );
+
+		$this->assertTrue( is_array( $array ) );
+		$this->assertSame( 1, count( $array ) );
+		$this->assertTrue( isset( $array['existing'] ) );
+		$this->assertSame( 2, count( $array['existing'] ) );
+	}
+
+	public function test_sanitize_size() {
+		// Default to px
+		$this->assertSame( '10px', PostTypeColumnHelper::sanitize_size( 10 ) );
+
+		// Doesn't override valid value
+		$this->assertSame( '10px', PostTypeColumnHelper::sanitize_size( '10px' ) );
+
+		// Ignore weird values
+		$this->assertSame( false, PostTypeColumnHelper::sanitize_size( false ) );
+		$this->assertSame( null, PostTypeColumnHelper::sanitize_size( null ) );
 	}
 }
