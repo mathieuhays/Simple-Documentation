2ba303f80873190a5cdeb650dfdcf723da2570d5
Mathieu <mathieu@mathieuhays.com>
2017-09-07 22:12:24 +0100

    sketch plan to automate release to wp.org

---
+++ bin/release-to-wp.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#
+# @TODO Release to WordPress Plugin Repo
+#
+# 1. Create new release with git flow
+# 2. Clone latest release in tmp folder
+# 3. Run svn commands etc..
+#
+# Ideas:
+# - automate release increment?
+# - automate generation of readme's change log?
+#
