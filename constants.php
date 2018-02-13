40032f15e238165bf6adf3668c4caed2a7b83d4b
Mathieu <mathieu@mathieuhays.com>
2018-02-13 23:35:04 +0000

    refactor file architecture

---
+++ constants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Simple Documentation - Constants
+ */
+
+/**
+ * Version Numbers
+ */
+define( 'SIMPLE_DOCUMENTATION_VERSION', '1.2.6' );
+
+
+/**
+ * Paths
+ */
+define( 'SIMPLE_DOCUMENTATION_DIR', dirname( __FILE__ ) );
+define( 'SIMPLE_DOCUMENTATION_LANGUAGES', SIMPLE_DOCUMENTATION_DIR . '/languages' );
+define( 'SIMPLE_DOCUMENTATION_VIEWS', SIMPLE_DOCUMENTATION_DIR . '/views' );
+
---
+++ includes/namespace.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Simple Documentation
+ */
+
+namespace Simple_Documentation;
+
+
+/**
+ * Classes
+ */
+require_once 'class-simple-documentation.php';
+
+
+
+/**
+ * Bootstrap
+ */
+new Simple_Documentation();
